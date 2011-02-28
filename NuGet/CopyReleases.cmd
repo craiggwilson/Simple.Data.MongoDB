73283b6f2f72df4c1378df60c41aef33f26e4a44
craiggwilson <craiggwilson@gmail.com>
2011-02-28 17:36:25 -0600

    updated nuget spec and generated 0.5.0.2

--- NuGet/CopyReleases.cmd
+++ NuGet/CopyReleases.cmd
@@ -1,4 +1,3 @@
-copy /y ..\Simple.Data.MongoDB\bin\Release\Simple.Data.dll Simple.Data.MongoDB\lib\net40
 copy /y ..\Simple.Data.MongoDB\bin\Release\Simple.Data.MongoDB.dll Simple.Data.MongoDB\lib\net40
 copy /y ..\Simple.Data.MongoDB\bin\Release\MongoDB.Bson.dll Simple.Data.MongoDB\lib\net40
 copy /y ..\Simple.Data.MongoDB\bin\Release\MongoDB.Driver.dll Simple.Data.MongoDB\lib\net40
